356dc1af7998c0679cf4012ca8104fccd7807e03
Hemantcods <hy564636@gmail.com>
2025-11-03 19:30:35 +0530

    remove styled AntSwitch definition and import from components

--- src/layouts/dashboard/SideBar.jsx
+++ src/layouts/dashboard/SideBar.jsx
@@ -4,8 +4,6 @@ import {
   Divider,
   IconButton,
   Stack,
-  styled,
-  Switch,
   useTheme,
 } from "@mui/material";
 import { Gear } from "phosphor-react";
@@ -14,59 +12,12 @@ import Logo from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data/index.jsx";
 import { faker } from "@faker-js/faker";
 import useSettings from "../../hooks/useSettings";
+import AntSwitch from "../../components/AntSwitch.jsx";
 
 function SideBar() {
   const theme = useTheme();
   const { onToggleMode } = useSettings();
   const [index, setIndex] = useState();
-
-  const AntSwitch = styled(Switch)(({ theme }) => ({
-    width: 40,
-    height: 20,
-    padding: 1,
-    display: "flex",
-    "&:active": {
-      "& .MuiSwitch-thumb": {
-        width: 40,
-      },
-      "& .MuiSwitch-switchBase.Mui-checked": {
-        transform: "translateX(9px)",
-      },
-    },
-    "& .MuiSwitch-switchBase": {
-      padding: 2,
-      "&.Mui-checked": {
-        transform: "translateX(20px)",
-        color: "#fff",
-        "& + .MuiSwitch-track": {
-          opacity: 1,
-          backgroundColor: "#1890ff",
-          ...theme.applyStyles("dark", {
-            backgroundColor: "#177ddc",
-          }),
-        },
-      },
-    },
-    "& .MuiSwitch-thumb": {
-      boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
-      width: 16,
-      height: 16,
-      borderRadius: 8,
-      transition: theme.transitions.create(["width"], {
-        duration: 200,
-      }),
-    },
-    "& .MuiSwitch-track": {
-      borderRadius: 20 / 2,
-      opacity: 1,
-      backgroundColor: "rgba(0,0,0,.25)",
-      boxSizing: "border-box",
-      ...theme.applyStyles("dark", {
-        backgroundColor: "rgba(255,255,255,.35)",
-      }),
-    },
-  }));
-
   return (
     <Box
       p={2}
