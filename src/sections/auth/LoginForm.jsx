72af07367f3bf7c2bee9060aa5fa2b5819f29f68
Hemantcods <hy564636@gmail.com>
2025-11-14 01:01:41 +0530

    Login Page completed

--- src/sections/auth/LoginForm.jsx
+++ src/sections/auth/LoginForm.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider, { RHFTextField } from "../../components/hook-form";
+import { useForm } from "react-hook-form";
+import {
+  Alert,
+  Button,
+  IconButton,
+  InputAdornment,
+  Link,
+  Stack,
+} from "@mui/material";
+import { Eye, EyeClosed } from "phosphor-react";
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid Email address"),
+    password: Yup.string().required("Password is Required"),
+  });
+  const defaultValues = {
+    email: "example@email.com",
+    password: "demo@123",
+  };
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
 
-export default LoginForm
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      // Submit data to backend
+    } catch (error) {
+      console.log(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+
+        <RHFTextField name="email" label="Email address" />
+        <RHFTextField
+          name="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? <Eye /> : <EyeClosed />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+      <Stack alignItems={"flex-end"} sx={{ my: 2 }}>
+        <Link variant="body2" color="inherit" underline="always">
+          Forgot Password?
+        </Link>
+      </Stack>
+      <Button fullWidth color="inherit" size="large" type="submit" variant="contained" sx={{bgcolor:"text.primary",color:(theme)=>theme.palette.mode==="light"?"common.white":"grey.800","&hover":{
+        bgcolor:"text.primary",
+        color:(theme)=>theme.palette.mode==="light"?"common.white":"grey.800"
+      }}}>
+        Login
+      </Button>
+    </FormProvider>
+  );
+};
+
+export default LoginForm;
